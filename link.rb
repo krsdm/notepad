b2b33d4851591346bd76607fbd44b1f273ee74fb
krsdm <krsdm@ya.ru>
2021-05-27 23:43:47 +0300

    created Post, Link, Memo and Task classes

---
+++ link.rb
@@ -0,0 +1,16 @@
+class Link < Post
+
+  def initialize
+    super # вызывает конструктор родителя, будут созданы такие же поля
+    @url = ''
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+
+end
---
+++ memo.rb
@@ -0,0 +1,13 @@
+class Memo < Post
+
+  # если ну указывать конструктор, будет вызван конструктор родителя
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+
+end
